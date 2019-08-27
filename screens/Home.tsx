38bcecce8be6986f7867e1021f0ae541503321ac
unknown <thiago.bardella@gmail.com>
2019-08-27 08:05:12 -0300

    d

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -3,18 +3,27 @@ import {
   SafeAreaView,
   StatusBar
 } from 'react-native';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+ export const client = new ApolloClient({
+  uri: 'https://tq-template-server-sample.herokuapp.com/graphql',
+});
 
 import { Login } from './Login';
 
 const Home = (props) => {
 
   return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <Login navigation={props.navigation}/>
-      </SafeAreaView>
-    </Fragment>
+    <ApolloProvider client={client}>    
+      <Fragment>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <Login navigation={props.navigation}/>
+        </SafeAreaView>
+      </Fragment>
+    </ApolloProvider>
+
   );
 };
 
--- screens/UsersPage.tsx
+++ screens/UsersPage.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
+import ApolloClient from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { gql } from "apollo-boost";
+import AsyncStorage from '@react-native-community/async-storage';
+import { Query } from "react-apollo";
+import { useQuery, ApolloProvider } from '@apollo/react-hooks';
+
+
+interface UsersPageProps {
+  navigation: any;
+}
+
+export interface UsersPageState {
+  token: string;
+}
+
 
 const users = [
   {
@@ -20,24 +36,170 @@ const users = [
   }
 ];
 
-export class UsersPage extends React.Component {
+const usersQuery = gql`
+  {
+    Users {
+      nodes {
+        name
+        email
+      }
+    }
+  }
+`;
+
+import { createHttpLink } from "apollo-link-http";
+
+
+
+// a query with apollo-client
+
+function UsersList() {
+  const { loading, error, data } = useQuery(usersQuery);
+
+  // if (loading) return <Text>Loading...</Text>;
+  if (error) return <Text>`Error :( ${error.message}`</Text>;
+  if (data) return <Text>Data retrieved</Text>
+
+
+
+
+  return (
+  // <Text>This is useQuery </Text>
+    data.Users.nodes.sort((a, b) => (a.name > b.name) ? 1 : -1).map(
+      user => {
+        (
+          <View style={styles.wrapper}>
+            <Text style={styles.title}>{user.name}</Text>
+            <Text style={styles.detail}>{user.email}</Text>
+          </View>
+        );
+  }));
+}
+
+export class UsersPage extends React.Component<UsersPageProps, UsersPageState> {
+  constructor (props: UsersPageProps) {
+    super(props);
+
+    this.state = {
+        token: ""
+              }
+  }
+
+  // private onLoad = (token: string) => {
+  //   this.getToken();
+
+  //   // const Dogs = ({ onDogSelected }) => (
+  //   //   return <Query query={usersQuery}>
+  //   //     {({ loading, error, data }) => {
+  //   //       if (loading) return "Loading...";
+  //   //       if (error) return `Error! ${error.message}`;
+
+  //   //        <Text>dfsfdsdf</Text>
+  //   //         // {data.sort((a, b) => (a.userName > b.userName) ? 1 : -1).map(user => {
+  //   //         //   return (
+  //   //         //     <View style={styles.wrapper}>
+  //   //         //       <Text style={styles.title}>{user.name}</Text>
+  //   //         //       <Text style={styles.detail}>{user.email}</Text>
+  //   //         //     </View>
+  //   //         //   );
+  //   //         // })}
+          
+  //   //     }}
+  //   // </Query>);
+
+
+
+  //   const result = client.query({
+  //     query: usersQuery,
+  //     context: {
+  //       headers: {
+  //         Authorization: token
+  //       }
+  //     }
+  //   });
+  //   return <Text>LOOK. SUCCESSFUL QUERY </Text>;
+  //   // .then(result => {
+  //   //   // result.data.Users.nodes.sort((a, b) => (a.name > b.name) ? 1 : -1).map(
+  //   //   //   user => {
+  //   //   //     return (
+  //   //   //       <View style={styles.wrapper}>
+  //   //   //         <Text style={styles.title}>{user.userName}</Text>
+  //   //   //         <Text style={styles.detail}>{user.email}</Text>
+  //   //   //       </View>
+  //   //   //     );
+  //   //   //   }
+  //   //   // )
+  //   //   return <Text>LOOK. SUCCESSFUL QUERY</Text>
+  //   // }).catch(err => this.setState({token : "FAILED QUERY"}));
+  // }
+
+  private getToken = () => {
+    AsyncStorage.getItem('token')
+      .then((value) => this.setState({ token: value || 'none' }))
+      .catch(err => this.props.navigation.navigate('Home'));
+      return this.state.token;
+  }
+
   render() {
-    return (
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={styles.scrollView}>
-        <View style={styles.body}>
-          {users.sort((a, b) => (a.userName > b.userName) ? 1 : -1).map(user => {
-            return (
-              <View style={styles.wrapper}>
-                <Text style={styles.title}>{user.userName}</Text>
-                <Text style={styles.detail}>{user.email}</Text>
-              </View>
-            );
-          })}
-        </View>
-      </ScrollView>
-    );
+    const headers = {
+      Authorization: this.getToken()
+    }
+
+    const link = createHttpLink({ uri: "https://tq-template-server-sample.herokuapp.com/graphql" , headers: headers});
+
+    const client = new ApolloClient({
+      link: link,
+      cache: new InMemoryCache()
+    });
+    
+    try {
+      const result = client.query({
+        query: usersQuery
+      }); 
+      // <Text>Query executed </Text>;
+      return (
+      <Text>This is useQuery {JSON.stringify(result)}</Text> );
+        //   result.data.Users.nodes.sort((a, b) => (a.name > b.name) ? 1 : -1).map(
+        //     user => {
+        //       (
+        //         <View style={styles.wrapper}>
+        //           <Text style={styles.title}>{user.name}</Text>
+        //           <Text style={styles.detail}>{user.email}</Text>
+        //         </View>
+        //       );
+        // }));
+
+    } catch(error) {
+      return <Text>ERROR</Text>;
+    }
+    
+    // return (<ApolloProvider client={client}>
+    //   <UsersList />
+
+    // </ApolloProvider>);
+    
+    // (<Query query={usersQuery}>
+    //           {() => <Text>Here we are</Text>}
+    //       </Query>);
+
+    // return (
+    //   <ScrollView
+    //     contentInsetAdjustmentBehavior="automatic"
+    //     style={styles.scrollView}>
+    //     <View style={styles.body}>
+    //       {/* {users.sort((a, b) => (a.userName > b.userName) ? 1 : -1).map(user => {
+    //         return (
+    //           <View style={styles.wrapper}>
+    //             <Text style={styles.title}>{user.userName}</Text>
+    //             <Text style={styles.detail}>{user.email}</Text>
+    //           </View>
+    //         );
+    //       })} */}
+    //       {this.onLoad(this.state.token)}
+    //       <Text>FIM - {this.state.token}</Text>
+    //     </View>
+    //   </ScrollView>
+    // );
   }
 }
 
